8c021e8ee46b946df6d7bf45b5ebc9358f105c04
Martin Barth <martin@senfdax.de>
2018-03-23 17:35:11 +0100

    ajust view automatically - better error handling

--- src/main.rs
+++ src/main.rs
@@ -7,7 +7,12 @@ use sdl2::keyboard::Keycode;
 use sdl2::rect::Point;
 use std::time::Duration;
 use std::cell::RefCell;
+use std::error::Error;
 use rand::distributions::{Range, Sample};
+
+const WINDOW_WIDTH: u32 = 1200;
+const WINDOW_HEIGHT: u32 = 800;
+const NUM_OF_MATTER: u32 = 90;
 
 struct MainGame {
     canvas: sdl2::render::WindowCanvas,
@@ -48,16 +53,19 @@ impl Vec2D {
 
 
 impl Matter {
-    fn new(pos_x: f64, pos_y: f64) -> Matter {
+    fn new(pos_x: f64, pos_y: f64, mass: f64) -> Matter {
         Matter {
             velocity: Vec2D { x: 0f64 , y: 0f64 },
             pos: Vec2D { x: pos_x , y: pos_y },
-            mass: 1.0,
+            mass: mass,
         }
     }
 
-    fn apply_force(&mut self, force: &Vec2D) {
-        self.velocity.add_vec(force);
+    fn apply_force(&mut self, mut force: Vec2D) {
+        // F = m * a; => a = F / m
+        // convert force to "a"
+        force.scale( 1.0 / self.mass );
+        self.velocity.add_vec(&force);
     }
 
     fn move_around(&mut self) {
@@ -66,41 +74,73 @@ impl Matter {
 }
 
 impl MainGame {
-    fn new() -> MainGame {
+    fn new() -> Result<MainGame, String> {
         let sdl_context = sdl2::init().unwrap();
-        let video_subsystem = sdl_context.video().unwrap();
-        let window = video_subsystem.window("gravity1", 1401, 1401)
+        let video_subsystem = sdl_context.video()?;
+        let window = video_subsystem.window("gravity1", WINDOW_WIDTH, WINDOW_HEIGHT)
             .position_centered()
             .build()
             .unwrap();
-        let canvas = window.into_canvas().build().unwrap();
-        let event_pump = sdl_context.event_pump().unwrap();
-        MainGame {canvas, event_pump}
+        let canvas = match window.into_canvas().build() {
+            Ok(canvas) => canvas,
+            Err(e) => return Err(e.description().to_string()),
+        };
+        let event_pump = sdl_context.event_pump()?;
+        Ok(MainGame {canvas, event_pump})
     }
 }
 
-fn to_canvas_pos(pos_x: f64, pos_y: f64) -> (i32, i32) {
-    let x = 801 + pos_x as i32;
-    let y = 801 + pos_y as i32;
-    return (x, y);
+struct Viewport {
+    x_shift: i32,
+    y_shift: i32,
 }
+
+impl Viewport {
+    fn new() -> Self {
+        Viewport {
+            x_shift: WINDOW_WIDTH as i32 / 2,
+            y_shift: WINDOW_HEIGHT as i32 / 2,
+        }
+    }
+
+    fn to_canvas_pos(&self, pos: &Vec2D) -> (i32, i32) {
+        let x = self.x_shift + pos.x as i32;
+        let y = self.y_shift + pos.y as i32;
+        return (x, y);
+    }
+
+    // fn shift_view(&mut self, x: i32, y: i32) {
+    //     self.x_shift += x;
+    //     self.y_shift += y;
+    // }
+
+    fn adjust_to_drift(&mut self, drift: &Vec2D) {
+        self.x_shift = WINDOW_WIDTH as i32  / 2 - drift.x as i32;
+        self.y_shift = WINDOW_HEIGHT as i32 / 2 - drift.y as i32;
+    }
+}
+
 
 fn main() {
 
-    let mut game = MainGame::new();
-    
+    let mut game = MainGame::new().unwrap();
+
     game.canvas.set_draw_color(Color::RGB(0, 0, 0));
     game.canvas.clear();
     game.canvas.present();
     let mut space = Range::new(-280f64, 280f64);
+    let mut weight = Range::new(0.1f64, 100.0f64);
     let mut rng = rand::thread_rng();
-    
+
     let mut matter: Vec<RefCell<Matter>> = Vec::new();
-    for _ in 0..15 {
+    for _ in 0..NUM_OF_MATTER {
         let pos_x = space.sample(&mut rng);
         let pos_y = space.sample(&mut rng);
-        matter.push( RefCell::new(Matter::new(pos_x, pos_y)) );
+        let mass  = weight.sample(&mut rng);
+        matter.push( RefCell::new(Matter::new(pos_x, pos_y, mass)) );
     }
+
+    let mut viewport = Viewport::new();
 
     'running: loop {
         // ! calculate next step
@@ -114,26 +154,38 @@ fn main() {
                 from_m_to_other.del_vec(&m.pos);
                 let distance = from_m_to_other.length();
                 // force = g * m1 * m2 / r*r
-                let force_factor = 0.2 * m.mass * other.mass / (distance * distance);
+                let force_factor = 0.003 * m.mass * other.mass / (distance * distance);
                 from_m_to_other.scale(force_factor);
                 force.add_vec(&from_m_to_other);
             }
-            m.apply_force(&force);
+            m.apply_force(force);
             m.move_around();
         }
-        
+
         // ! draw screen
         // black screen
         game.canvas.set_draw_color(Color::RGB(0, 0, 0));
         game.canvas.clear();
+        game.canvas.set_draw_color(Color::RGB(255,255, 0));
+        let mut viewdrift = Vec2D { x: 0.0 , y: 0.0 };
+        let (x, y) = viewport.to_canvas_pos(&viewdrift);
+        game.canvas.draw_point(Point::new(x, y)).unwrap();
         // draw matter
         game.canvas.set_draw_color(Color::RGB(255, 255, 255));
         for matter in matter.iter() {
             let m = matter.borrow();
-            let (x, y) = to_canvas_pos(m.pos.x, m.pos.y);
+            let pos = &m.pos;
+            viewdrift.add_vec(pos);
+            let (x, y) = viewport.to_canvas_pos(pos);
             // println!("drawing white point to {} {}", x, y);
             game.canvas.draw_point(Point::new(x, y)).unwrap();
         }
+        game.canvas.set_draw_color(Color::RGB(255,0,0));
+        viewdrift.scale(1.0 / matter.len() as f64);
+        let (x, y) = viewport.to_canvas_pos(&viewdrift);
+        game.canvas.draw_point(Point::new(x, y)).unwrap();
+        viewport.adjust_to_drift(&viewdrift);
+        println!("viewdrift is {} {}", viewdrift.x, viewdrift.y);
         // update Screen
         game.canvas.present();
 
@@ -143,11 +195,23 @@ fn main() {
                 Event::KeyDown { keycode: Some(Keycode::Escape), ..} => {
                     break 'running
                 }
+                // Event::KeyDown { keycode: Some(Keycode::W), ..} => {
+                //     viewport.shift_view(0, 50);
+                // }
+                // Event::KeyDown { keycode: Some(Keycode::A), ..} => {
+                //     viewport.shift_view(50, 0);
+                // }
+                // Event::KeyDown { keycode: Some(Keycode::S), ..} => {
+                //     viewport.shift_view(0, -50);
+                // }
+                // Event::KeyDown { keycode: Some(Keycode::D), ..} => {
+                //     viewport.shift_view(-50, 0);
+                // }
                 _ => {}
             }
         }
 
-        game.canvas.present();
         ::std::thread::sleep(Duration::new(0, 1_000_000_000u32 / 60 ));
+        //::std::thread::sleep(Duration::new(1, 0 ));
     }
 }
